86485d8580389fe491a0f558ab434939e7f767a6
ANUBOLURUPENDRAREDDY <2300033687@kluniversity.in>
2025-09-26 19:55:49 +0530

    first commit

---
+++ frontendreactapp/src/main/About.jsx
@@ -0,0 +1,17 @@
+export default function About() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '20px' }}>
+      <h2>About Our Event Management System</h2>
+      <p style={{ maxWidth: '600px', margin: '20px auto', lineHeight: '1.6' }}>
+        Our Event Management System is a platform designed to simplify and 
+        automate the planning, organization, and execution of events. 
+        It provides features such as event scheduling, registration management, 
+        attendee tracking, notifications, and reporting. The goal is to 
+        enhance user experience for both organizers and participants by 
+        offering a seamless and efficient system for managing events of all scales.
+      </p>
+    
+      
+    </div>
+  );
+}
---
+++ frontendreactapp/vite.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  base: '/eventtmanagement/',   // <-- Add this line (your repo name)
+  plugins: [react()],
+  server: {
+    port: 2000
+  }
+})
